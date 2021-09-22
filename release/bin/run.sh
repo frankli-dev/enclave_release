4b09df5f88383d886e50a74fe1a9c941ed684cc8
Frank Li <frankli.consult@gmail.com>
2021-09-21 23:05:52 -0400

    first commit

---
+++ release/bin/run.sh
@@ -0,0 +1,5 @@
+./substratee-worker init-shard
+./substratee-worker shielding-key
+./substratee-worker signing-key
+./substratee-worker mrenclave > ~/mrenclave.b58
+ ./substratee-worker -P 2094 -p 9994 run --skip-ra
